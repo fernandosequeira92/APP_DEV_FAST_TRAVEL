dacd6ce1502fba504eb66befa53a257960dcc0d1
Fernando Sequeira <fernandosequeira92@outlook.com>
2026-09-14 18:38:03 -0300

    Correção

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -5,6 +5,7 @@ import { ChIntArtificial } from '../childrens/ch-int-artificial/ch-int-artificia
 import { ChMultimidia } from '../childrens/ch-multimidia/ch-multimidia';
 import { ChRepositorio } from "../childrens/ch-repositorio/ch-repositorio";
 import { ChFerramentas } from "../childrens/ch-ferramentas/ch-ferramentas";
+import { ChIndex } from '../childrens/ch-index/ch-index';
 
 
 @Component({
@@ -16,10 +17,11 @@ import { ChFerramentas } from "../childrens/ch-ferramentas/ch-ferramentas";
     ChMultimidia, 
     ChRepositorio, 
     ChFerramentas,
+    ChIndex
   ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
-  @Input() menuAtual: string = 'developer';
+  @Input() menuAtual: string = 'index';
 }
